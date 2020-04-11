3f91b5a2cf79ae26bc319ab402430556fe7d66b8
Ethan Tam <ethantam623@hotmail.com>
2020-04-10 20:03:40 -0700

    minor linting changes to codebase

--- js/legend.js
+++ js/legend.js
@@ -1,108 +1,99 @@
 const initializeLegend = () => {
   let parentSvg = d3.select('svg#legend');
 
-  let group = parentSvg.append('g')
-    .attr('transform', 'translate(0, 10)');
-  group.append('text')
+  let group = parentSvg.append('g').attr('transform', 'translate(0, 10)');
+  group
+    .append('text')
     .attr('x', 0)
     .attr('y', 14)
     .attr('font-size', 12)
     .attr('font-weight', 'bold')
     .text('Link Thickness');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 30)');
-  group.append('line')
-      .attr('x1', 0)
-      .attr('y1', 10)
-      .attr('x2', 20)
-      .attr('y2', 10)
-      .attr('stroke-width', 1)
-      .attr('stroke', 'lightgrey')
-      .attr('fill', 'none')
-  group.append('text')
-      .attr('x', 30)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .text('actor has 1 movie in genre')
+  group = parentSvg.append('g').attr('transform', 'translate(0, 30)');
+  group
+    .append('line')
+    .attr('x1', 0)
+    .attr('y1', 10)
+    .attr('x2', 20)
+    .attr('y2', 10)
+    .attr('stroke-width', 1)
+    .attr('stroke', 'lightgrey')
+    .attr('fill', 'none');
+  group
+    .append('text')
+    .attr('x', 30)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .text('actor has 1 movie in genre');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 50)');
-  group.append('line')
-      .attr('x1', 0)
-      .attr('y1', 10)
-      .attr('x2', 20)
-      .attr('y2', 10)
-      .attr('stroke-width', 3.5)
-      .attr('stroke', 'lightgrey')
-      .attr('fill', 'none')
-  group.append('text')
-      .attr('x', 30)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .text('actor has 5 movies in genre')
+  group = parentSvg.append('g').attr('transform', 'translate(0, 50)');
+  group
+    .append('line')
+    .attr('x1', 0)
+    .attr('y1', 10)
+    .attr('x2', 20)
+    .attr('y2', 10)
+    .attr('stroke-width', 3.5)
+    .attr('stroke', 'lightgrey')
+    .attr('fill', 'none');
+  group
+    .append('text')
+    .attr('x', 30)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .text('actor has 5 movies in genre');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 70)');
-  group.append('line')
-      .attr('x1', 0)
-      .attr('y1', 10)
-      .attr('x2', 20)
-      .attr('y2', 10)
-      .attr('stroke-width', 6)
-      .attr('stroke', 'lightgrey')
-      .attr('fill', 'none')
-  group.append('text')
-      .attr('x', 30)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .text('actor has 9 movies in genre')
+  group = parentSvg.append('g').attr('transform', 'translate(0, 70)');
+  group
+    .append('line')
+    .attr('x1', 0)
+    .attr('y1', 10)
+    .attr('x2', 20)
+    .attr('y2', 10)
+    .attr('stroke-width', 6)
+    .attr('stroke', 'lightgrey')
+    .attr('fill', 'none');
+  group
+    .append('text')
+    .attr('x', 30)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .text('actor has 9 movies in genre');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 100)');
-  group.append('text')
-      .attr('x', 0)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .attr('font-weight', 'bold')
-      .text('Circle Radius');
+  group = parentSvg.append('g').attr('transform', 'translate(0, 100)');
+  group
+    .append('text')
+    .attr('x', 0)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .attr('font-weight', 'bold')
+    .text('Circle Radius');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 120)');
-  group.append('circle')
-      .attr('cx', 10)
-      .attr('cy', 10)
-      .attr('r', 3)
-      .attr('fill', 'grey')
-  group.append('text')
-      .attr('x', 30)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .text('actor has 1 movie total')
+  group = parentSvg.append('g').attr('transform', 'translate(0, 120)');
+  group.append('circle').attr('cx', 10).attr('cy', 10).attr('r', 3).attr('fill', 'grey');
+  group
+    .append('text')
+    .attr('x', 30)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .text('actor has 1 movie total');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 140)');
-  group.append('circle')
-      .attr('cx', 10)
-      .attr('cy', 10)
-      .attr('r', 5)
-      .attr('fill', 'grey')
-  group.append('text')
-      .attr('x', 30)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .text('actor has 8 movies total')
+  group = parentSvg.append('g').attr('transform', 'translate(0, 140)');
+  group.append('circle').attr('cx', 10).attr('cy', 10).attr('r', 5).attr('fill', 'grey');
+  group
+    .append('text')
+    .attr('x', 30)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .text('actor has 8 movies total');
 
-  group = parentSvg.append('g')
-      .attr('transform', 'translate(0, 160)');
-  group.append('circle')
-      .attr('cx', 10)
-      .attr('cy', 10)
-      .attr('r', 7)
-      .attr('fill', 'grey')
-  group.append('text')
-      .attr('x', 30)
-      .attr('y', 14)
-      .attr('font-size', 12)
-      .text('actor has 15 movies total')
-}
+  group = parentSvg.append('g').attr('transform', 'translate(0, 160)');
+  group.append('circle').attr('cx', 10).attr('cy', 10).attr('r', 7).attr('fill', 'grey');
+  group
+    .append('text')
+    .attr('x', 30)
+    .attr('y', 14)
+    .attr('font-size', 12)
+    .text('actor has 15 movies total');
+};
--- js/process_data.js
+++ js/process_data.js
@@ -6,28 +6,39 @@ const processData = () => {
   let actorToYearGenre = {};
   let actorToGenreCount = {};
   // initialize actorToYearGenre with "all" object
-  actorToYearGenre["all"] = [{year: 2006},{year: 2007},{year: 2008},{year: 2009},{year: 2010},{year: 2011},
-                                  {year: 2012},{year: 2013},{year: 2014},{year: 2015},{year: 2016}];
-  actorToGenreCount["all"] = makeGenreCountRow("All Genres");
+  actorToYearGenre['all'] = [
+    { year: 2006 },
+    { year: 2007 },
+    { year: 2008 },
+    { year: 2009 },
+    { year: 2010 },
+    { year: 2011 },
+    { year: 2012 },
+    { year: 2013 },
+    { year: 2014 },
+    { year: 2015 },
+    { year: 2016 },
+  ];
+  actorToGenreCount['all'] = makeGenreCountRow('All Genres');
 
   d3.csv('data/movie-data.csv').then(data => {
     // Parse data into separate dicts
     data.forEach(d => {
       // Get d's "main" genre
-      g = d['Genre'].split(',')[0]
-      let index = +d['Year']-2006
+      g = d['Genre'].split(',')[0];
+      let index = +d['Year'] - 2006;
       // Deal with actorToYearGenre for "all"
-      if (actorToYearGenre["all"][index].hasOwnProperty(g)) {
-          actorToYearGenre["all"][index][g]++;
-        } else {
-          actorToYearGenre["all"][index][g] = 1;
-        }
+      if (actorToYearGenre['all'][index].hasOwnProperty(g)) {
+        actorToYearGenre['all'][index][g]++;
+      } else {
+        actorToYearGenre['all'][index][g] = 1;
+      }
       // Deal with actorToGenreCount for "all"
-      addOrIncrementSeriesValues(actorToGenreCount, index, g, "all");
+      addOrIncrementSeriesValues(actorToGenreCount, index, g, 'all');
       // Deal with actorToGenreCount for genres
       if (!(g in actorToGenreCount)) {
-          actorToGenreCount[g] = makeGenreCountRow(g);
-        }
+        actorToGenreCount[g] = makeGenreCountRow(g);
+      }
       addOrIncrementSeriesValues(actorToGenreCount, index, g, g);
 
       // Everything has to do with actors, so make them the outer loop
@@ -45,7 +56,7 @@ const processData = () => {
 
         // Handle the actor to genre dict
         if (!(name in actorToGenres)) {
-          actorToGenres[name] = [{genre: g, count: 1}];
+          actorToGenres[name] = [{ genre: g, count: 1 }];
         } else {
           let isNew = true;
           actorToGenres[name].forEach(t => {
@@ -54,8 +65,7 @@ const processData = () => {
               isNew = false;
             }
           });
-          if (isNew)
-            actorToGenres[name].push({genre: g, count: 1});
+          if (isNew) actorToGenres[name].push({ genre: g, count: 1 });
         }
 
         // Handle the actor to actor dict
@@ -72,15 +82,25 @@ const processData = () => {
                 isNew = false;
               }
             });
-            if (isNew)
-              actorToActor[name].push({name: otherName, count: 1});
+            if (isNew) actorToActor[name].push({ name: otherName, count: 1 });
           }
         });
 
         // Handle the actor to genre-year dict
         if (!(name in actorToYearGenre)) {
-          actorToYearGenre[name] = [{year: 2006},{year: 2007},{year: 2008},{year: 2009},{year: 2010},{year: 2011},
-                                  {year: 2012},{year: 2013},{year: 2014},{year: 2015},{year: 2016}];
+          actorToYearGenre[name] = [
+            { year: 2006 },
+            { year: 2007 },
+            { year: 2008 },
+            { year: 2009 },
+            { year: 2010 },
+            { year: 2011 },
+            { year: 2012 },
+            { year: 2013 },
+            { year: 2014 },
+            { year: 2015 },
+            { year: 2016 },
+          ];
         }
         if (actorToYearGenre[name][index].hasOwnProperty(g)) {
           actorToYearGenre[name][index][g]++;
@@ -102,30 +122,30 @@ const processData = () => {
     let aToARows = [];
 
     Object.entries(genreToActors).forEach(([k, v]) => {
-      let row = {genre: k, actors: v};
+      let row = { genre: k, actors: v };
       gToARows.push(row);
     });
     gToARows.sort((a, b) => {
-      return b.actors.length - a.actors.length
+      return b.actors.length - a.actors.length;
     });
 
     Object.entries(actorToGenres).forEach(([k, v]) => {
-      let row = {actor: k, genres: v};
+      let row = { actor: k, genres: v };
       aToGRows.push(row);
     });
     aToGRows.sort((a, b) => {
-      return b.genres.length - a.genres.length
+      return b.genres.length - a.genres.length;
     });
 
     Object.entries(actorToActor).forEach(([k, v]) => {
       v.sort((a, b) => {
         return b.count - a.count;
       });
-      let row = {actor: k, actors: v};
+      let row = { actor: k, actors: v };
       aToARows.push(row);
     });
     aToARows.sort((a, b) => {
-      return b.actors.length - a.actors.length
+      return b.actors.length - a.actors.length;
     });
 
     // Log the strings so they can be pasted into CSV files
@@ -135,30 +155,32 @@ const processData = () => {
     // console.log(actorToYearGenre);
     // console.log(actorToGenreCount);
   });
-}
+};
 
 // Helper function to add a series row or increment the correct count
 function addOrIncrementSeriesValues(actorToGenreCount, yearIndex, g, entityName) {
-  let seriesIdx = actorToGenreCount[entityName]["series"].map(row => row["name"]).indexOf(g);
+  let seriesIdx = actorToGenreCount[entityName]['series'].map(row => row['name']).indexOf(g);
   if (seriesIdx == -1) {
     // if row doesn't exist yet, push row to seires
-    actorToGenreCount[entityName]["series"].push(makeSeriesRow(g, yearIndex));
+    actorToGenreCount[entityName]['series'].push(makeSeriesRow(g, yearIndex));
   } else {
     // else increment correct year of existing row
-    actorToGenreCount[entityName]["series"][seriesIdx]["values"][yearIndex]++;
+    actorToGenreCount[entityName]['series'][seriesIdx]['values'][yearIndex]++;
   }
 }
 
 // Simple helper to create genre count row with y = entityName
 function makeGenreCountRow(entityName) {
-  return {y: entityName, 
-          series: [],
-          dates: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]};
+  return {
+    y: entityName,
+    series: [],
+    dates: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+  };
 }
 
 // Simple helper to create series row with given genrename and 1 at given yearIndex
 function makeSeriesRow(genreName, yearIndex) {
-  let row = {name: genreName, values: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]};
-  row["values"][yearIndex] = 1;
+  let row = { name: genreName, values: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] };
+  row['values'][yearIndex] = 1;
   return row;
 }
